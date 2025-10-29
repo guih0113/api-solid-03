9ee05d52dcce3238f6cc8d892832e65ac88078d0
guih0113 <gui.henriquess13@gmail.com>
2025-10-29 14:56:27 -0300

    debug: environment variables response

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import { app } from './app'
 import { env } from './env'
 
+console.log('DATABASE_URL from env:', process.env.DATABASE_URL)
+
 app.listen({
   host: '0.0.0.0',
-  port: env.PORT
+  port: env.PORT,
 }).then(() => {
   console.log('🚀 HTTP Server Running!')
 })
